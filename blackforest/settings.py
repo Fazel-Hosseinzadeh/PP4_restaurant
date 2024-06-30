a9cdc5bfe730683afeed7df78d7760bd5a4601c2
Fazel HosseinzadehDastak <33155934+Fazel-Hosseinzadeh@users.noreply.github.com>
2024-06-30 17:24:57 +0200

    Add SECRET_KEY within env.py and set it within settings.py

--- blackforest/settings.py
+++ blackforest/settings.py
@@ -25,12 +25,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6c9p!)lb-(b4xssw%w)0o8%#i-c=)%jlxuc_5&y2g$x9f0wldb'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = [
+    '127.0.0.1',
     '.herokuapp.com',
 ]
 
