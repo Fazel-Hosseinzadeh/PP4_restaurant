c16bbf3b27c901773f31f22ef86aa59666db9a63
Fazel HosseinzadehDastak <33155934+Fazel-Hosseinzadeh@users.noreply.github.com>
2024-07-13 17:27:55 +0200

    Modify  app home views

--- home/views.py
+++ home/views.py
@@ -4,6 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return render (
-                    request,
+        request,
         "home/home.html",
                    )
