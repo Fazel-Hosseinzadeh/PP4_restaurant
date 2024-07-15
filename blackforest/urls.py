b96ea5bebb913b719ddec983ee2fd28777906584
Fazel HosseinzadehDastak <33155934+Fazel-Hosseinzadeh@users.noreply.github.com>
2024-07-15 11:30:06 +0200

    Add urls for allauth urls

--- blackforest/urls.py
+++ blackforest/urls.py
@@ -6,6 +6,7 @@ from home.views import home
 urlpatterns = [
     path("", include("home.urls"), name = 'home-urls'),
     path("", include("menus.urls"), name = 'menus-urls'),
+    path("accounts/", include("allauth.urls")),
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
 ]
