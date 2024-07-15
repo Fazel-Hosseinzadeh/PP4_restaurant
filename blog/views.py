53a3c8151be56c21f8bc4aa5880c6fd7e0b426af
Fazel HosseinzadehDastak <33155934+Fazel-Hosseinzadeh@users.noreply.github.com>
2024-07-15 18:18:16 +0200

    Add post detail function into blog app views

--- blog/views.py
+++ blog/views.py
@@ -9,3 +9,17 @@ class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
     template_name = "blog/blog.html"
     paginate_by = 3
+    
+# Post detail
+def post_detail(request, slug):
+    
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "blog/post_detail.html",
+        {
+            "post": post,
+        },
+    )
