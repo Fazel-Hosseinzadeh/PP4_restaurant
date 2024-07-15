5d458e3762d4dd94af903dcb45cc30e6edecdd74
Fazel HosseinzadehDastak <33155934+Fazel-Hosseinzadeh@users.noreply.github.com>
2024-07-15 16:04:06 +0200

    Add urls for blog app and wire it up to project

--- blackforest/urls.py
+++ blackforest/urls.py
@@ -6,6 +6,7 @@ from home.views import home
 urlpatterns = [
     path("", include("home.urls"), name = 'home-urls'),
     path("menus/", include("menus.urls"), name = 'menus-urls'),
+    path("post/", include("blog.urls"), name="blog-urls"),
     path("accounts/", include("allauth.urls")),
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
