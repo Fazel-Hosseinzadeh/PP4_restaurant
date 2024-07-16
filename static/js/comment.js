2d6e4789386d4ad431cf2130458df94b032dd740
Fazel HosseinzadehDastak <33155934+Fazel-Hosseinzadeh@users.noreply.github.com>
2024-07-16 12:20:53 +0200

    Add js for calling modal and deleting a comment

--- static/js/comment.js
+++ static/js/comment.js
@@ -1,9 +1,23 @@
+/**
+ * Variabels related to editing comment
+ */
 const editButtons = document.getElementsByClassName("btn-edit");
 const commentText = document.getElementById("id_body");
 const commentForm = document.getElementById("commentForm");
 const submitButton = document.getElementById("submitButton");
 
+/**
+ * Variabels related to deleting comment
+ */
+const deleteModal = new bootstrap.Modal(document.getElementById("deleteModal"));
+const deleteButtons = document.getElementsByClassName("btn-delete");
+const deleteConfirm = document.getElementById("deleteConfirm");
 
+
+
+/**
+ * Editing comments
+ */
 for (let button of editButtons) {
     button.addEventListener("click", (e) => {
         let commentId = e.target.getAttribute("data-comment_id");
@@ -15,3 +29,14 @@ for (let button of editButtons) {
     });
 }
 
+/**
+ * Deleting comments
+ */
+
+for (let button of deleteButtons) {
+    button.addEventListener("click", (e) => {
+        let commentId = e.target.getAttribute("data-comment_id");
+        deleteConfirm.href = `delete_comment/${commentId}`;
+        deleteModal.show();
+    });
+}
