6cab6d1e1c9ae2ddce8d458cc5230f2747942d65
Fazel HosseinzadehDastak <33155934+Fazel-Hosseinzadeh@users.noreply.github.com>
2024-07-16 14:13:15 +0200

    Add table model into book app

--- book/models.py
+++ book/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Table model
+class Table(models.Model):
+
+    table_name = models.CharField(
+        max_length=50,
+        default='Table',
+        unique=True
+        )
+    max_seats = models.PositiveIntegerField(default=4)
+
+    class Meta:
+        ordering = ['-max_seats']
+
+    def __str__(self):
+        return f"Table  {self.table_name} | Max seats {self.max_seats}"
