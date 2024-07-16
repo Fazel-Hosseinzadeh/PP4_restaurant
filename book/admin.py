cff0889c37a4b634e04056c1da9843918bbbfd85
Fazel HosseinzadehDastak <33155934+Fazel-Hosseinzadeh@users.noreply.github.com>
2024-07-16 14:34:40 +0200

    Add table model to admin panel

--- book/admin.py
+++ book/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Table, Booking
 
-# Register your models here.
+# Register Table model
+@admin.register(Table)
+class TableAdmin(admin.ModelAdmin):
+    list_display = ('table_id', 'table_name', 'max_seats')
