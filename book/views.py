c3276fae362f0959fca5ed853d438f7a86634857
Fazel HosseinzadehDastak <33155934+Fazel-Hosseinzadeh@users.noreply.github.com>
2024-07-16 20:42:59 +0200

    Add vies for booking

--- book/views.py
+++ book/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from .models import Table
+from .forms import BookingForm
 
 # Create your views here.
 def book(request):
+    tables= Table.objects.all()
+    booking_form = BookingForm()
+    
     return render (
         request,
         "book/book.html",
+        {'booking_form': booking_form}
                    )
