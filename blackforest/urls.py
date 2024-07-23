83c874fcad7002fb6e5da166752f1e40a7012d07
Fazel HosseinzadehDastak <33155934+Fazel-Hosseinzadeh@users.noreply.github.com>
2024-07-23 12:25:43 +0200

    Add links and urls to wire up contact us

--- blackforest/urls.py
+++ blackforest/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path("menus/", include("menus.urls"), name = 'menus-urls'),
     path("blog/", include("blog.urls"), name="blog-urls"),
     path("book/", include("book.urls"), name="book-urls"),
+    path("contact/", include("contact.urls"), name="contact-urls"),
     path("accounts/", include("allauth.urls")),
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+# Contact views
+def contact(request):
+    return render (
+        request,
+        "contact/contact.html",
+                   )
