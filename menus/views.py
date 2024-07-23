c62bf7e980ce3631514b86e9fdd1be0ff1d0ade9
Fazel HosseinzadehDastak <33155934+Fazel-Hosseinzadeh@users.noreply.github.com>
2024-07-23 23:19:49 +0200

    Add docstring to views in the menus app

--- menus/views.py
+++ menus/views.py
@@ -2,6 +2,23 @@ from django.shortcuts import render
 from .models import FoodItem, DrinkItem
 
 def menus(request):
+    """
+    Retrieves and displays food and drink menus.
+
+    Fetches all `FoodItem` and `DrinkItem` instances from the database and passes them to the template for display.
+    This view provides a comprehensive list of available food and drink items on the menu.
+
+    **Context**
+
+    ``food_menu``
+        All instances of :model:`app.FoodItem`.
+    ``drink_menu``
+        All instances of :model:`app.DrinkItem`.
+
+    **Template:**
+
+    :template:`menus/menus.html`
+    """
     food_menu = FoodItem.objects.all()
     drink_menu = DrinkItem.objects.all()
     return render(
