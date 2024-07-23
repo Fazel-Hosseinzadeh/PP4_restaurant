6695fda84e5ac91e3ebfdf122157aac40f964e91
Fazel HosseinzadehDastak <33155934+Fazel-Hosseinzadeh@users.noreply.github.com>
2024-07-23 09:49:12 +0200

    Modify views book function to send correct message

--- book/views.py
+++ book/views.py
@@ -20,59 +20,34 @@ def book(request):
             booking.phone = request.POST.get('phone')
             booking.status = 'Awaiting confirmation'
             booking.guest_count = request.POST.get('guest_count')
-            booking.table_reserved = request.POST.get('table')
+            booking.table = Table.objects.get(id=request.POST.get('table'))  # Correct assignment of table
             booking.requested_date = request.POST.get('requested_date')
             booking.requested_time  = request.POST.get('requested_time')
-            
-            other_bookings_book = False #for checking if others booked already
-            if bookings:
-                for other_booking in bookings:
-                    if other_booking.table == booking.table and (
-                        (other_booking.requested_date == booking.requested_date and
-                        other_booking.requested_time >= booking.requested_time and
-                        other_booking.requested_time < (booking.requested_time + timedelta(hours=2)))):
-                        other_bookings_book = True
-                        
-            if not other_bookings_book:
-                booking.save()
-                messages.add_message(
-                    request, messages.SUCCESS,
-                    'Booking submitted successfully and awaiting approval'
-                )
-                #update bookings list with new booking 
-                bookings = Booking.objects.all()
-                
-            else:
-                messages.add_message(
-                    request, messages.SUCCESS,
-                    'This table is alredy booked '
-                )
-                
-                        
-                       
+            booking.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Booking submitted successfully and awaiting approval'
+            )
         else:
-            
-            # Giving feedback of the reason of failure
-            if not booking_form.is_valid():
-                for error in booking_form.errors:
-                    if error == '__all__':
-                        continue
+            # Giving feedback on the reason for failure
+            for field, errors in booking_form.errors.items():
+                for error in errors:
                     messages.add_message(
                         request, messages.ERROR,
-                        f'Error in {error} field. Booking unsuccessful'
+                        f'Error in {field} field: {error}'
                     )
-                    
-                    
-    booking_form = BookingForm()  
-    
-    return render (
+
+    booking_form = BookingForm()
+
+    return render(
         request,
         "book/book.html",
         {
             'booking_form': booking_form,
-            'bookings' : bookings,
-         }
-                   )
+            'bookings': bookings,
+        }
+    )
+
     
 # Edit booking function
 def edit(request , id):
