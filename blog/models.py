7e9fed028eb7252d12bd1822081f6e2dba8ebb64
Fazel HosseinzadehDastak <33155934+Fazel-Hosseinzadeh@users.noreply.github.com>
2024-07-23 21:35:51 +0200

    Add docstring for models in blog app

--- blog/models.py
+++ blog/models.py
@@ -2,10 +2,24 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
-# Create your models here.
 
+# Choices for the status of a post
 STATUS = ((0, "Draft"), (1, "Published"))
 class Post(models.Model):
+    """
+     Stores a single blog post entry related to :model:`auth.User`.
+
+    Attributes:
+        title (str): The title of the post, must be unique.
+        slug (str): The URL-friendly version of the title, must be unique.
+        author (User): The user who authored the post.
+        featured_image (CloudinaryField): The image featured in the post, default is 'placeholder'.
+        content (str): The main content of the post.
+        created_on (datetime): The date and time when the post was created.
+        status (int): The status of the post, either 'Draft' (0) or 'Published' (1).
+        excerpt (str): A short summary of the post.
+        updated_on (datetime): The date and time when the post was last updated.
+    """
 
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
@@ -27,6 +41,16 @@ class Post(models.Model):
     
     
 class Comment(models.Model):
+    """
+    Stores a single comment entry related to :model:`auth.User` and :model:`Post`.
+
+    Attributes:
+        post (ForeignKey): The post that the comment is related to.
+        author (ForeignKey): The user who wrote the comment.
+        body (TextField): The content of the comment.
+        created_on (DateTimeField): The date and time when the comment was created.
+        approved (BooleanField): Whether the comment is approved for display.
+    """
     post = models.ForeignKey(Post, on_delete=models.CASCADE,
                              related_name="comments")
     author = models.ForeignKey(
