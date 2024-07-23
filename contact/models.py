f49b2930979d0110f97a3827b50558b177671f7e
Fazel HosseinzadehDastak <33155934+Fazel-Hosseinzadeh@users.noreply.github.com>
2024-07-23 13:14:56 +0200

    Add and migrate models for contact app

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class ContactContent(models.Model):
+    title = models.CharField(max_length=200)
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+class ContactMessage(models.Model):
+
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    message = models.TextField()
+    read = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Message from {self.name}"
