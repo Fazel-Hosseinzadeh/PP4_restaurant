b51e6c8f8c83a8141c6a37d5516e6d36f24f1f75
Fazel HosseinzadehDastak <33155934+Fazel-Hosseinzadeh@users.noreply.github.com>
2024-07-23 22:57:05 +0200

    Add docstring to forms in book app

--- contact/forms.py
+++ contact/forms.py
@@ -3,7 +3,23 @@ from .models import ContactMessage
 
 
 class ContactForm(forms.ModelForm):
-    
+    """
+    Form class for users to send contact messages.
+
+    Provides a form for users to submit their name, email, and a message. This form is used to capture
+    contact messages from users and create `ContactMessage` instances.
+
+    **Fields**
+    ``name``
+        A text input field for the user's name.
+    ``email``
+        A text input field for the user's email address.
+    ``message``
+        A text area field for the user's message.
+
+    **Model**
+    :model:`app.ContactMessage`
+    """
     class Meta:
 
         model = ContactMessage
