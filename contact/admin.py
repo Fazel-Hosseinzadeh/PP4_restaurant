3699fa81bb30cc14a3e84b581a6dbcc7b2385638
Fazel HosseinzadehDastak <33155934+Fazel-Hosseinzadeh@users.noreply.github.com>
2024-07-23 14:44:22 +0200

    Add contact message model into contact app admin and read action

--- contact/admin.py
+++ contact/admin.py
@@ -1,9 +1,33 @@
 from django.contrib import admin
-from .models import ContactContent
+from .models import ContactMessage, ContactContent
 from django_summernote.admin import SummernoteModelAdmin
 
-
+# Admin Contact Content
 @admin.register(ContactContent)
 class ContactContentAdmin(SummernoteModelAdmin):
 
     summernote_fields = ('content',)
+
+
+# Admin Contact Message
+@admin.register(ContactMessage)
+class ContactMessageAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'email',
+        'read',
+        )
+
+    search_fields = [
+                    'name',
+                    'email',
+                     ]
+    list_filter = (
+                    'read',
+                   )
+    actions = ['read']
+
+    # Add actions to do on the bookings
+    def read(self, request, queryset):
+        queryset.update(read=True)
+    
