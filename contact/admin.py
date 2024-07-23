872dee37bc69e780875509f87e3472ef8322ae06
Fazel HosseinzadehDastak <33155934+Fazel-Hosseinzadeh@users.noreply.github.com>
2024-07-23 22:55:33 +0200

    Add docstring to admin in book app

--- contact/admin.py
+++ contact/admin.py
@@ -2,16 +2,51 @@ from django.contrib import admin
 from .models import ContactMessage, ContactContent
 from django_summernote.admin import SummernoteModelAdmin
 
-# Admin Contact Content
+
 @admin.register(ContactContent)
 class ContactContentAdmin(SummernoteModelAdmin):
+    """
+    Admin interface for managing ContactContent.
+
+    Provides a rich-text editor for the `content` field using Summernote. This admin class allows users to
+    edit the content with enhanced text formatting options.
+
+    **Fields**
+    ``summernote_fields``
+        Specifies the fields to be rendered with the Summernote rich-text editor.
+
+    **Template**
+    None
+    """
 
     summernote_fields = ('content',)
 
 
-# Admin Contact Message
+
 @admin.register(ContactMessage)
 class ContactMessageAdmin(admin.ModelAdmin):
+    """
+    Admin interface for managing ContactMessage instances.
+
+    Displays key fields in the admin list view, provides search functionality, and allows filtering by
+    the read status. Includes a custom action to mark messages as read.
+
+    **Fields**
+    ``list_display``
+        Fields to display in the admin list view: `name`, `email`, and `read`.
+    ``search_fields``
+        Fields that can be searched: `name` and `email`.
+    ``list_filter``
+        Filters available in the admin list view: `read`.
+
+    **Actions**
+    ``read``
+        Custom action to mark selected messages as read.
+
+    **Template**
+    None
+    """
+    
     list_display = (
         'name',
         'email',
@@ -27,7 +62,12 @@ class ContactMessageAdmin(admin.ModelAdmin):
                    )
     actions = ['read']
 
-    # Add actions to do on the bookings
+
     def read(self, request, queryset):
+        """
+        Marks selected messages as read.
+
+        Updates the `read` field to `True` for the selected `ContactMessage` instances.
+        """
         queryset.update(read=True)
     
