67bf8dc697b77bb0aacf39416473aa34cfcdd701
Fazel HosseinzadehDastak <33155934+Fazel-Hosseinzadeh@users.noreply.github.com>
2024-07-23 21:30:42 +0200

    Add docstring to forms in blog app

--- blog/forms.py
+++ blog/forms.py
@@ -3,6 +3,16 @@ from .models import Comment
 
 
 class CommentForm(forms.ModelForm):
+    """
+    A form for creating and updating Comment instances.
+
+    This form uses the Django ModelForm to automatically generate form fields
+    based on the Comment model. It includes only the 'body' field.
+
+    Attributes:
+        model (Comment): The model associated with this form.
+        fields (tuple): The fields to include in the form.
+    """
     class Meta:
         model = Comment
         fields = ('body',)
