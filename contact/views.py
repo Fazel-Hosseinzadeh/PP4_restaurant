28c505dfd17961015c3ae56cf678f6a64516f5a7
Fazel HosseinzadehDastak <33155934+Fazel-Hosseinzadeh@users.noreply.github.com>
2024-07-23 13:28:09 +0200

    Modify contact views to show cotact content

--- contact/views.py
+++ contact/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
-
+from .models import ContactContent
 
 # Contact views
 def contact(request):
+    content = ContactContent.objects.all().order_by('-updated_on').first()
+    
+    
     return render (
         request,
         "contact/contact.html",
+        {
+        "content": content,
+        }
                    )
