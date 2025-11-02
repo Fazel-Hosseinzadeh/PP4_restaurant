4f292ab9e921947e371c680aa161dbb41853c6cf
Fazel HosseinzadehDastak <33155934+Fazel-Hosseinzadeh@users.noreply.github.com>
2025-11-02 19:04:27 +0100

    settingsprod miss spelling correction

--- blackforest/settingsprod.py
+++ blackforest/settingsprod.py
@@ -112,7 +112,7 @@ WSGI_APPLICATION = 'blackforest.wsgi.application'
 
 DATABASE = {
     'default': {
-        'ENGINE':'django.db.backend.postgresql_psycopg2',
+        'ENGINE':'django.db.backends.postgresql_psycopg2',
         'NAME': 'os.environ.get("DB_NAME")',
         'USER': 'os.environ.get("DB_USER")',
         'PASSWORD': 'os.environ.get("DB_PASSWORD")',
